a642fdee4eb8b487250a2c7047d42f1b5f2437c7
hyul77 <kimhyul77@gmail.com>
2024-01-11 16:06:36 +0900

    finish

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question, Answer
+from django.utils import timezone
+
+
 # Create your views here.
 
 
@@ -11,7 +14,11 @@ def index(request):
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'pybo/question_detail.html', {'question' : question})
 
-    
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get("content"), create_date=timezone.now())
+
+    return redirect('pybo:detail', question_id=question.id)
